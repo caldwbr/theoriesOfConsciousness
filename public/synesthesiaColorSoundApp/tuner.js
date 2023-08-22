6dfc223f35fa18749f778a70c441574079fa84b1
caldwbr <caldwbr@gmail.com>
2023-08-21 22:33:48 -0500

    fix buggy

--- public/synesthesiaColorSoundApp/tuner.js
+++ public/synesthesiaColorSoundApp/tuner.js
@@ -29,26 +29,28 @@ function init() {
   analyser.minDecibels = -100;
   analyser.maxDecibels = -10;
   analyser.smoothingTimeConstant = 0.85;
-  if (!navigator?.mediaDevices?.getUserMedia) {
-    // No audio allowed
-    alert('Sorry, getUserMedia is required for the app.')
-    return;
-  } else {
-    var constraints = {audio: true};
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(
-        function(stream) {
-          // Initialize the SourceNode
-          source = audioContext.createMediaStreamSource(stream);
+    
+  if (navigator && navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  // Code to handle getUserMedia
+  var constraints = {audio: true};
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(
+      function(stream) {
+        // Initialize the SourceNode
+        source = audioContext.createMediaStreamSource(stream);
           // Connect the source node to the analyzer
           source.connect(analyser);
           visualize();
-        }
-      )
-      .catch(function(err) {
-        alert('Sorry, microphone permissions are required for the app. Feel free to read on without playing :)')
-      });
-  }
+      }
+    )
+    .catch(function(err) {
+      alert('Sorry, microphone permissions are required for the app. Feel free to read on without playing :)')
+    });
+} else {
+  // No audio allowed
+  alert('Sorry, getUserMedia is required for the app.')
+  return;
+}
 
   // Visualizing, copied from voice change o matic
   var canvas = document.querySelector('.visualizer');
